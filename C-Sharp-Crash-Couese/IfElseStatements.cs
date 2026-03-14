18742d12691d28ae4f8ae9b70c9d5630dee85862
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-03-14 15:48:41 +0500

    If Else Statement in C#

---
+++ C-Sharp-Crash-Couese/IfElseStatements.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace C_Sharp_Crash_Couese
+{
+    internal class IfElseStatements
+    {
+        public void Show()
+        {
+            // If statement = a basic form of decision making that allows you to execute a block of code if a specified condition is true
+            /*
+            Console.WriteLine("Please Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age > 100)
+            {
+                Console.WriteLine("You are too old to sign up!");
+            }
+            else if (age >= 18 )
+            {
+                Console.WriteLine("You are now signed up!");
+            }
+            else if (age < 0)
+            {
+                Console.WriteLine("You haven't been born yet!");
+            }
+            else
+            {
+                Console.WriteLine("You must be 18+ to sign up!");
+            }
+            */
+            Console.WriteLine("Please Enter your age: ");
+            String name = (Console.ReadLine());
+
+            //if(name == "")
+            if(name != "")
+            {
+                //Console.WriteLine("You did not enter your name!");
+                Console.WriteLine("Hello " + name + "!");
+            }
+            else
+            {
+                //Console.WriteLine("Hello " + name + "!");
+                Console.WriteLine("You did not enter your name!");
+            }
+        }
+    }
+}
