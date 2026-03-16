7e970c7b4c0af9aaaca3dff82179f16397b833f9
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-03-16 15:53:48 +0500

    Logical Operators in C#

---
+++ C-Sharp-Crash-Couese/LogicalOperators.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.CodeDom.Compiler;
+
+namespace C_Sharp_Crash_Couese
+{
+    internal class LogicalOperators
+    {
+        public void Show()
+        {
+            // Logical operators = Can be used to check of more then 1 condition is true /false
+            // && = AND operator (both conditions must be true)
+            // || = OR operator (one of the conditions must be true)
+
+            Console.WriteLine("Whats the temperature outside: (C)");
+            double temp = Convert.ToDouble(Console.ReadLine());
+          
+            if (temp >= 10 && temp <= 25)
+            {
+                Console.WriteLine("It's worm outside!");
+            }
+            else if (temp <= -50 || temp >= 50 )
+            {
+                Console.WriteLine("DON'T GO OUTSIDE!");
+            }
+        }   
+    }
+}
