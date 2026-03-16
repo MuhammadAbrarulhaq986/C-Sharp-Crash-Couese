12d7faf590631f5b1952755fb071905e6181ecb7
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-03-16 16:07:40 +0500

    While loops in C#

---
+++ C-Sharp-Crash-Couese/WhileLoops.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C_Sharp_Crash_Couese
+{
+    internal class WhileLoops
+    {
+        public void Show() 
+        {
+            // While loop = reqeats some code while some condition is true
+
+            //Console.Write("Enter your name: ");
+            //String name = Console.ReadLine();
+            String name = "";
+
+            while (name == "") 
+            {
+                Console.Write("Enter your name: ");
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
