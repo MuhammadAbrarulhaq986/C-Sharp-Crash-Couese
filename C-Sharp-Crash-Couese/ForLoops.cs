8cd8bd8a1e44d99ff7ab45740be148553dfd5f2d
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-03-17 21:13:17 +0500

    For Loop in C#

---
+++ C-Sharp-Crash-Couese/ForLoops.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace C_Sharp_Crash_Couese
+{
+    internal class ForLoops
+    {
+        public void Show() {
+
+            // For Loop = Repeats some code a FINITE amount of times
+            /*
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+             */
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Happy New Year!");
+        }
+    }
+}
