6b9525b69424f632540e904e22889b4c0a04fee1
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-03-19 15:53:00 +0500

    Nested Loops in C#

---
+++ C-Sharp-Crash-Couese/NestedLoops.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace C_Sharp_Crash_Couese
+{
+    internal class NestedLoops
+    {
+        public void Show()
+        {
+            /* Nested loops = loops inside of other loops User vary.
+                              Used a lot in sorting algorithms and 2D arrays (arrays inside of arrays) 
+                        */
+            Console.Write("How many rows ?: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("How many columns ?: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("What symbol ?: ");
+            String symbol = Console.ReadLine();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(symbol);
+                }
+                    Console.WriteLine();
+            }
+
+        }
+    }
+}
