6eede8666feb59a10a818b873d8e3ee49b92462d
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-03-19 16:20:59 +0500

    Number Guessing Game using Nested loops in C#

---
+++ C-Sharp-Crash-Couese/NumberGuessing.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace C_Sharp_Crash_Couese
+{
+    internal class NumberGuessing
+    {
+        public void Show() 
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int number;
+            int guesses;
+            String response;
+
+            while (playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                response = "";
+                number = random.Next(min, max + 1);
+
+                while (guess !=  number)
+                {
+                    Console.WriteLine("Guess a number between " + min + " - " + max + " : ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess: " + guess);
+
+                    if (guess > number)
+                    {
+                        Console.WriteLine(guess +  " is to high!");
+                    }
+                    else if (guess < number)
+                    {
+                        Console.WriteLine(guess + " is to low!");
+                    }
+                    guesses++;
+                }
+                Console.WriteLine("Number: " + number);
+                Console.WriteLine("Congrats You Win!");
+                Console.WriteLine("Guesses: " + guesses);
+
+                Console.WriteLine("Would you like to play again (Y/N): ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "Y")
+                {
+                    playAgain = true;
+                }
+                else
+                {
+                    playAgain = false;
+                }
+            }
+            Console.WriteLine("Thanks for playing!");
+        }
+    }
+}
