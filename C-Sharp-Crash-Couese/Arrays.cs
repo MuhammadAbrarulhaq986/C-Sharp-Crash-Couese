885c123b3da0c289f029912ecef430b4d6513a8f
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-03-22 18:06:08 +0500

    Arrays in C# and iteration using a For loop,  For Each Loop on the array elements

---
+++ C-Sharp-Crash-Couese/Arrays.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace C_Sharp_Crash_Couese
+{
+    internal class Arrays
+    {
+        public void Show()
+        {
+            // Array = A variable that can store multiple values, Fixed size, of the same data type
+
+            String[] cars = new string[3];
+
+            //String[] cars = { "BMW", "Mustang", "Corvatta" };
+
+            cars[0] = "Tesla";
+            cars[1] = "BMW";
+            cars[2] = "Mustang";
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+
+            //Console.WriteLine(cars[0]);
+            //Console.WriteLine(cars[1]);
+            //Console.WriteLine(cars[2]);
+            //Console.WriteLine(cars[3]);
+        }
+    }
+}
---
+++ C-Sharp-Crash-Couese/ForEachLoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C_Sharp_Crash_Couese
+{
+    internal class ForEachLoop
+    {
+        public void Show()
+        {
+            // ForEach loop = A simpler way to iterate over an array, but Its less flexible than a for loop
+
+            String[] cars = { "BMW", "Mustang", "Corvette" };
+
+            //for (int i = 0; i < cars.Length; i++)
+            //{
+            //    Console.WriteLine(cars[i]);   
+            //}
+
+            foreach (String car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
