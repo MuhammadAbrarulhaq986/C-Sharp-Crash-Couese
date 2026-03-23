29db7bba1db61ed616cac7eb6e4be6a24ffb8316
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-03-23 12:31:59 +0500

    Return Keyword = returns data back to the place where a method is Invoked (called). in C#

---
+++ C-Sharp-Crash-Couese/ReturnKeyword.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace C_Sharp_Crash_Couese
+{
+    internal class ReturnKeyword
+    {
+        public void Show()
+        {
+            // Return = returns data back to the place where a method is Invoked (called)
+
+            double x, y;
+            double result;
+
+            Console.WriteLine("Enter in number 1: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Enter in number 2: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            result = Multiply(x,y);
+
+            Console.WriteLine(result);
+        }
+
+        static double Multiply(double x, double y)
+        {
+            double z = x * y;
+            return z;
+        }
+    }
+}
