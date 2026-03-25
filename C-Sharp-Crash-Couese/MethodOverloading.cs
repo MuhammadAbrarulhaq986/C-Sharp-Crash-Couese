1805ac2e370dcdffbc5fce0cd5aba4d2986b94ae
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-03-25 17:53:59 +0500

    Method overloading Needs a unique signature.                                                            Params Keyword that takes a variable number of arguments.

---
+++ C-Sharp-Crash-Couese/MethodOverloading.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace C_Sharp_Crash_Couese
+{
+    internal class MethodOverloading
+    {
+        public void Show()
+        {
+            /*
+             Method overloading = methods share same name, but different parameters
+                                  name + parameters = signature
+                                  methods must have a unique signature */
+            double total;
+
+            total = Multiply(2, 3, 4);
+
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+        /* 
+          Move this OUTSIDE of the Show() method
+          This is now a class member (static method)
+        */
+        static double Multiply(double a, double b)
+        {
+                return a * b;
+        }
+        static double Multiply(double a, double b, double c)
+        {
+                return a * b * c;
+        }
+
+    }
+}
---
+++ C-Sharp-Crash-Couese/ParamsKeyword.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace C_Sharp_Crash_Couese
+{
+    internal class ParamsKeyword
+    {
+        public void Show() 
+        {
+            /*
+            Params Keyword = A method parameter that takes a variable number of arguments.
+                             The parameter type must be a single-dimension array and it must be the last parameter in the method.
+            */
+            double total = CheckOut(3.99, 5.75, 15, 1.00, 10.25);
+            
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+        static double CheckOut(params double[] prices)
+        {
+            double total = 0;
+
+            foreach (double price in prices)
+            {
+                total += price; 
+            }
+            return total;
+        }
+    }
+}
