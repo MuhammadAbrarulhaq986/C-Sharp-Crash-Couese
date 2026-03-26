fdefc58f37a906e81fe939a5958e25e17b2be218
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-03-26 23:53:25 +0500

    Exception Handling in C# : Errors that occur during exections, Use for those dangerous lines of code that might give an error

---
+++ C-Sharp-Crash-Couese/ExceptionHandling .cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace C_Sharp_Crash_Couese
+{
+     class ExceptionHandling
+    {
+        public void Show()
+        {
+            /*
+            Exception = Errors that occur during exections
+                      try = try some code that is considered "Dangerous" 
+                      catch = catches and handles exceptions when they occur
+                      finally = always executes regardless if exceptions is caught or not
+            */
+            double x;
+              double  y;
+            double result;
+
+            try
+            {
+                Console.WriteLine("Enter numbar 1: ");
+                x = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter numbar 2: ");
+                y = Convert.ToInt32(Console.ReadLine());
+
+                result = x / y;
+
+                Console.WriteLine("Result: " + result);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Please enter only numbers!");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("You Can not Divide by Zero! Stupid!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+            finally 
+            {  
+                Console.WriteLine("Thanks for using this program!");
+            }
+        }
+    }
+}
+
