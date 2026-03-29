0925adcbd130604e8303b14160d6c634ea22b45e
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-03-29 16:44:58 +0500

    Classes A bundle of related code. Can be used as a blueprint to create objects (OOP)

---
+++ C-Sharp-Crash-Couese/Classes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace C_Sharp_Crash_Couese
+{
+    internal class Classes
+    {
+        public void Show()
+        {
+            /*
+            Class = A bundle of related code.
+                    Can be used as a blueprint to create objects (OOP)
+            */
+
+            Messages.Hello();
+            Messages.Waiting();
+            Messages.Bye();
+        }
+    }
+}
