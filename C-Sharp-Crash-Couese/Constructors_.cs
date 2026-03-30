3e46c39bef71f3a6d45587aa395322e79ee05ef6
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-03-30 11:12:46 +0500

    Objects = An Instance of a class can be used as a blueprint to create objects (OOP). Objects can have fields & methods.  Constructor = A special method in a class, Can be used to assign arguments to fields when creating an oblject

---
+++ C-Sharp-Crash-Couese/Constructors_.cs
@@ -0,0 +1,85 @@
+﻿using System;
+namespace C_Sharp_Crash_Couese
+{
+    internal class Constructors_
+    {
+        public void Show()
+        {
+            /* Constructor = A special method in a class 
+             *               Same name as the class name
+             *               Can be used to assign arguments to fields when creating an oblject  
+            */
+
+
+
+            //Human human1 = new Human("Gojo", 25);
+            //Human human2 = new Human("Yuji" , 20);
+
+            Car car1 = new Car("Ford", "Mustang", 2022, "Red");
+            Car car2 = new Car("Mazda", "RX-8", 2018, "Red");
+
+            car1.Drive();
+            car2.Drive();
+
+            /*
+            human1.name = "Gojo";
+            human1.age = 45;
+            //----- When we assign a constructor to a class,
+            ------- we don't need to assign the values to the fields like this,
+            ------- we can do it in the constructor itself.
+            human2.name = "Yuji";
+            human2.age = 20;
+            */
+            /*
+            human1.Eat();
+            human1.Sleep();
+            Console.WriteLine();
+            human2.Eat();
+            human2.Sleep();
+             */
+
+        }
+    }
+    class Car // This is a class named Car
+    {
+        String make;
+        String model;
+        int year;
+        String color;
+        public Car(String make, String model, int year, String color)// This is a constructor for the Car class
+        { 
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.color = color;
+        }
+        public void Drive()// This is a method for the Car class
+        {
+            //Console.WriteLine("You drive the " + make + " " + model);
+            Console.WriteLine($"You drive the {make} {model}");
+
+        }
+    }
+    /*
+    class Human
+    {
+        public String name;
+        public int age;
+        public Human(String n, int a) 
+        {
+            this.name = n;
+            this.age = a;
+
+        }
+        public void Eat()
+        {
+            Console.WriteLine(name + " is Eating...");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine(name + " is Sleep...");
+        }
+
+    }
+     */
+}
---
+++ C-Sharp-Crash-Couese/Objects_.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO.Pipes;
+namespace C_Sharp_Crash_Couese
+{
+    internal class Objects_
+    {
+        public  void SHow()
+        {
+        /*
+        Objects = An Instance of a class
+                  A class can be used as a blueprint to create objects (OOP)
+                  Objects can have fields & methods (Characteristics & actions)
+        */ 
+
+            Humen humen1 = new Humen();
+            Humen humen2 = new Humen();
+
+            humen1.name = "Gojo";
+            humen1.age = 45;
+
+            humen2.name = "Yuji";
+            humen2.age = 20;
+
+
+            humen1.Eat();
+            humen1.Sleep();
+            Console.WriteLine();
+            humen2.Eat();
+            humen2.Sleep();
+
+        }
+    }
+    class Humen 
+    {
+        public String name;
+        public int age;
+
+        public void Eat() {
+        Console.WriteLine(name +  " is Eating...");
+        }
+        public void Sleep() {
+        Console.WriteLine(name +  " is Sleep...");
+        }
+    }
+}
