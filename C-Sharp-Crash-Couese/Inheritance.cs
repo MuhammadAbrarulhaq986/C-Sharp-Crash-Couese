7b0c3baf41091c144d04933c7585bd843a4e1e86
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-04-02 21:38:42 +0500

    Inheritance = One or more child classes reciving fields, methods, etc, from a common parent

---
+++ C-Sharp-Crash-Couese/Inheritance.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace C_Sharp_Crash_Couese
+{
+    internal class Inheritance
+    {
+        public void Show() 
+        {
+            /* Inheritance = One or more child classes reciving fields, methods, etc, from a common parent */
+       
+            Car1 car = new Car1();
+        
+            Bicycle bicycle = new Bicycle();
+        
+            Boat boat = new Boat();
+
+            Console.WriteLine(car.speed);
+            Console.WriteLine(car.wheels);
+            car.go();
+            
+            Console.WriteLine(bicycle.speed);
+            Console.WriteLine(bicycle.wheels);
+            bicycle.go();
+            
+            Console.WriteLine(boat.speed);
+            Console.WriteLine(boat.wheels);
+            boat.go();
+        
+        }
+    }
+    class Vehicle 
+    {
+        public int speed = 0;
+        public void go() // Method
+        {
+            Console.WriteLine("This vehicle is moving!");
+        }
+    }
+    class Car1 : Vehicle 
+    {
+        public int wheels = 4;
+    }
+    class Bicycle : Vehicle 
+    {
+        public int wheels = 2;
+    }
+    class Boat : Vehicle 
+    {
+        public int wheels = 0;
+    }
+
+}
