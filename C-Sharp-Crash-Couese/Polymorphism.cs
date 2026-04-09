562b09e907ec45742d215878fe51284adba4bf4b
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-04-09 20:49:53 +0500

    Polymorphism = Greek word means to "Have many froms" Ojects can be identified by more then one type, Ex: A Dog is also : Canice , Animal, Organism

---
+++ C-Sharp-Crash-Couese/Polymorphism.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace C_Sharp_Crash_Couese
+{
+    internal class Polymorphism
+    {
+        public void Show()
+        {
+            /* Polymorphism = Greek word means to "Have many froms"
+                              Ojects can be identified by more then one type 
+                              Ex: A Dog is also : Canice , Animal, Organism
+            */
+
+            Car5 car = new Car5();
+            Bycycle5 bycycle = new Bycycle5();
+            Boat5 boat = new Boat5();
+
+            Vehicle5[] vechicles = { car, bycycle, boat };
+
+            foreach (Vehicle5 vehicle in vechicles)
+            {
+                vehicle.Go();
+            }
+        }
+    }
+    class Vehicle5
+    {
+        public virtual void Go() 
+        {
+        }
+    }
+    class Car5 : Vehicle5
+    {
+        public override void  Go()
+        {
+            Console.WriteLine("The Car is Moving!");
+        }
+    }
+    class Bycycle5 : Vehicle5
+    {
+        public override void Go()
+        {
+            Console.WriteLine("The Bycycle is Moving!");
+        }
+    }
+    class Boat5 : Vehicle5
+    {
+        public override void Go()
+        {
+            Console.WriteLine("The Boat is Moving!");
+        }
+    }
+}
