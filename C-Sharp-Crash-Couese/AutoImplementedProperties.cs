99691b3864d928ccefb10000181eeca6961cc7f1
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-04-14 21:18:57 +0500

    Auto-Implemented Properties  = shortcut when no additional logic is required in the property you do not have to define a field for a property, you only have to write get; and/or set; inside the property

---
+++ C-Sharp-Crash-Couese/AutoImplementedProperties.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace C_Sharp_Crash_Couese
+{
+    internal class AutoImplementedProperties
+    {
+        public void Show() 
+        {
+            /* Auto-Implemented Properties  = shortcut when no additional logic is required in the property
+                                              you do not have to define a field for a property,
+                                              you only have to write get; and/or set; inside the property */
+
+            Car8 car  = new Car8 ("BMW");
+            Console.WriteLine(car.Model);   
+        }
+    }
+    class Car8 
+    {
+        /*
+        String model;
+        
+        public String Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        */
+        public String Model { get; set; }// Auto-Implemented Property
+        public Car8(String model) // Constructor
+        {
+            this.Model = model;
+        }
+        
+    }
+}
